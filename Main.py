55e7172e85317acf6b4a5ee4fde3ac360a222223
Arash847 <100713493+Arash847@users.noreply.github.com>
2025-04-08 14:12:13 +0330

    Main.py

--- Main.py
+++ Main.py
@@ -5,20 +5,21 @@ import asyncio
 import pytz
 from keep_alive import keep_alive
 
-api_id = 24711413  # جایگزین کن با api_id خودت
-api_hash = '10e258eafb4f66acf2f829cd3819dc7f'  # جایگزین کن با api_hash خودت
-phone_number = '+989335398546'  # شماره موبایلت با +98
+api_id = 24711413  # جایگزین با api_id خودت
+api_hash = '10e258eafb4f66acf2f829cd3819dc7f'  # جایگزین با api_hash خودت
+phone = '+989335398546'  # شماره‌ات با +98
 
 client = TelegramClient('session', api_id, api_hash)
 
-async def update_name_clock():
-    await client.start(phone=phone_number)
+async def update_clock():
+    await client.start(phone=phone)
     while True:
         tehran_time = datetime.now(pytz.timezone('Asia/Tehran')).strftime('%H:%M')
-        await client(UpdateProfile(first_name=f'ArashR {tehran_time}'))
+        display_name = f".𝑨𝒓𝒂𝒔𝒉𝑹. {tehran_time}"
+        await client(UpdateProfile(first_name=display_name))
         await asyncio.sleep(60)
 
 keep_alive()
 
 with client:
-    client.loop.run_until_complete(update_name_clock())
+    client.loop.run_until_complete(update_clock())
