e0c4bf2ef8679cc81a6095fefcad7b2ea9daf70c
Arash847 <100713493+Arash847@users.noreply.github.com>
2025-04-08 13:51:25 +0330

    Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from keep_alive import keep_alive
 
 api_id = 24711413  # جایگزین کن با api_id خودت
 api_hash = '10e258eafb4f66acf2f829cd3819dc7f'  # جایگزین کن با api_hash خودت
-phone_number = '+98...'  # شماره موبایلت با +98
+phone_number = '+989335398546'  # شماره موبایلت با +98
 
 client = TelegramClient('session', api_id, api_hash)
 
